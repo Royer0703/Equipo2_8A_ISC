e51b6cbc54d0f7500c567bc00656817e716aa10f
adrian2521 <72817108+adrian2521@users.noreply.github.com>
2021-03-03 12:42:08 -0600

    Delete Cultivos.sql

--- TABLAS SQL/Cultivos.sql
+++
@@ -1,9 +0,0 @@
-Create database ERP
-use ERP
-
-create table SalesCultivos(
-    idCultivo   int primary key,
-	nombre   varchar(100) not null,
-	costoAsesoria float not null,
-	estatus  char not null,
-)
