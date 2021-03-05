e443b17845bd5f89af85d9908c5ea5b537d4503f
Royer0703 <61436502+Royer0703@users.noreply.github.com>
2021-03-05 11:57:14 -0600

    Login

--- Ventas_Equipo2/Vistas/LOGIN.cs
+++ Ventas_Equipo2/Vistas/LOGIN.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Vistas
 {
-    public partial class LOGIN : Form
+    public partial class Login : Form
     {
-        public LOGIN()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
