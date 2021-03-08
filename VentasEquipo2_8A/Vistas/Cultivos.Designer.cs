0bb7e2bb28d2792f07b01df6aa04048feff7e408
Royer0703 <61436502+Royer0703@users.noreply.github.com>
2021-03-07 22:38:40 -0600

    PaginaPrincipal

--- VentasEquipo2_8A/Vistas/Cultivos.Designer.cs
+++ VentasEquipo2_8A/Vistas/Cultivos.Designer.cs
@@ -53,9 +53,10 @@ namespace Vistas
             // 
             this.Cerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.Cerrar.Image = ((System.Drawing.Image)(resources.GetObject("Cerrar.Image")));
-            this.Cerrar.Location = new System.Drawing.Point(8, 8);
+            this.Cerrar.Location = new System.Drawing.Point(11, 10);
+            this.Cerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Cerrar.Name = "Cerrar";
-            this.Cerrar.Size = new System.Drawing.Size(24, 24);
+            this.Cerrar.Size = new System.Drawing.Size(32, 30);
             this.Cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Cerrar.TabIndex = 0;
             this.Cerrar.TabStop = false;
@@ -65,9 +66,10 @@ namespace Vistas
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(38, 12);
+            this.label1.Location = new System.Drawing.Point(51, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.Size = new System.Drawing.Size(180, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Listado De Cultivos";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -79,10 +81,12 @@ namespace Vistas
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
-            this.dataGridView1.Location = new System.Drawing.Point(42, 54);
+            this.dataGridView1.Location = new System.Drawing.Point(28, 66);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(506, 244);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(775, 368);
             this.dataGridView1.TabIndex = 2;
             // 
             // label2
@@ -90,9 +94,10 @@ namespace Vistas
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(577, 54);
+            this.label2.Location = new System.Drawing.Point(811, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.Size = new System.Drawing.Size(87, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "idCultivo";
             // 
@@ -101,9 +106,10 @@ namespace Vistas
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(577, 125);
+            this.label3.Location = new System.Drawing.Point(811, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.Size = new System.Drawing.Size(81, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "Nombre";
             // 
@@ -112,9 +118,10 @@ namespace Vistas
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(577, 194);
+            this.label4.Location = new System.Drawing.Point(811, 239);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(139, 20);
+            this.label4.Size = new System.Drawing.Size(173, 25);
             this.label4.TabIndex = 5;
             this.label4.Text = "Costo de Asesoria";
             // 
@@ -123,38 +130,43 @@ namespace Vistas
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(577, 251);
+            this.label5.Location = new System.Drawing.Point(811, 309);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.Size = new System.Drawing.Size(77, 25);
             this.label5.TabIndex = 6;
             this.label5.Text = "Estatus";
             // 
             // tbidCultivo
             // 
-            this.tbidCultivo.Location = new System.Drawing.Point(581, 90);
+            this.tbidCultivo.Location = new System.Drawing.Point(817, 111);
+            this.tbidCultivo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbidCultivo.Name = "tbidCultivo";
-            this.tbidCultivo.Size = new System.Drawing.Size(179, 20);
+            this.tbidCultivo.Size = new System.Drawing.Size(238, 22);
             this.tbidCultivo.TabIndex = 7;
             // 
             // tbNombre
             // 
-            this.tbNombre.Location = new System.Drawing.Point(581, 159);
+            this.tbNombre.Location = new System.Drawing.Point(817, 196);
+            this.tbNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbNombre.Name = "tbNombre";
-            this.tbNombre.Size = new System.Drawing.Size(176, 20);
+            this.tbNombre.Size = new System.Drawing.Size(234, 22);
             this.tbNombre.TabIndex = 8;
             // 
             // tbCostoAsesoria
             // 
-            this.tbCostoAsesoria.Location = new System.Drawing.Point(581, 217);
+            this.tbCostoAsesoria.Location = new System.Drawing.Point(817, 267);
+            this.tbCostoAsesoria.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbCostoAsesoria.Name = "tbCostoAsesoria";
-            this.tbCostoAsesoria.Size = new System.Drawing.Size(176, 20);
+            this.tbCostoAsesoria.Size = new System.Drawing.Size(234, 22);
             this.tbCostoAsesoria.TabIndex = 9;
             // 
             // tbEstatus
             // 
-            this.tbEstatus.Location = new System.Drawing.Point(581, 275);
+            this.tbEstatus.Location = new System.Drawing.Point(817, 338);
+            this.tbEstatus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbEstatus.Name = "tbEstatus";
-            this.tbEstatus.Size = new System.Drawing.Size(179, 20);
+            this.tbEstatus.Size = new System.Drawing.Size(238, 22);
             this.tbEstatus.TabIndex = 10;
             // 
             // btnNuevo
@@ -179,16 +191,17 @@ namespace Vistas
             this.btnNuevo.IconVisible = true;
             this.btnNuevo.IconZoom = 50D;
             this.btnNuevo.IsTab = false;
-            this.btnNuevo.Location = new System.Drawing.Point(27, 340);
+            this.btnNuevo.Location = new System.Drawing.Point(28, 474);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnNuevo.Name = "btnNuevo";
             this.btnNuevo.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
             this.btnNuevo.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
             this.btnNuevo.OnHoverTextColor = System.Drawing.Color.White;
             this.btnNuevo.selected = false;
-            this.btnNuevo.Size = new System.Drawing.Size(170, 40);
+            this.btnNuevo.Size = new System.Drawing.Size(227, 49);
             this.btnNuevo.TabIndex = 11;
             this.btnNuevo.Text = "NUEVO";
-            this.btnNuevo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNuevo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnNuevo.Textcolor = System.Drawing.Color.White;
             this.btnNuevo.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
@@ -214,16 +227,17 @@ namespace Vistas
             this.btnGuardar.IconVisible = true;
             this.btnGuardar.IconZoom = 50D;
             this.btnGuardar.IsTab = false;
-            this.btnGuardar.Location = new System.Drawing.Point(213, 340);
+            this.btnGuardar.Location = new System.Drawing.Point(276, 474);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
             this.btnGuardar.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
             this.btnGuardar.OnHoverTextColor = System.Drawing.Color.White;
             this.btnGuardar.selected = false;
-            this.btnGuardar.Size = new System.Drawing.Size(170, 40);
+            this.btnGuardar.Size = new System.Drawing.Size(227, 49);
             this.btnGuardar.TabIndex = 12;
             this.btnGuardar.Text = "GUARDAR";
-            this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnGuardar.Textcolor = System.Drawing.Color.White;
             this.btnGuardar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
@@ -249,16 +263,17 @@ namespace Vistas
             this.btnEditar.IconVisible = true;
             this.btnEditar.IconZoom = 50D;
             this.btnEditar.IsTab = false;
-            this.btnEditar.Location = new System.Drawing.Point(402, 340);
+            this.btnEditar.Location = new System.Drawing.Point(528, 474);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
             this.btnEditar.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
             this.btnEditar.OnHoverTextColor = System.Drawing.Color.White;
             this.btnEditar.selected = false;
-            this.btnEditar.Size = new System.Drawing.Size(170, 40);
+            this.btnEditar.Size = new System.Drawing.Size(227, 49);
             this.btnEditar.TabIndex = 13;
             this.btnEditar.Text = "EDITAR";
-            this.btnEditar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEditar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEditar.Textcolor = System.Drawing.Color.White;
             this.btnEditar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
@@ -284,25 +299,26 @@ namespace Vistas
             this.btnEliminar.IconVisible = true;
             this.btnEliminar.IconZoom = 50D;
             this.btnEliminar.IsTab = false;
-            this.btnEliminar.Location = new System.Drawing.Point(590, 340);
+            this.btnEliminar.Location = new System.Drawing.Point(779, 474);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
             this.btnEliminar.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
             this.btnEliminar.OnHoverTextColor = System.Drawing.Color.White;
             this.btnEliminar.selected = false;
-            this.btnEliminar.Size = new System.Drawing.Size(170, 40);
+            this.btnEliminar.Size = new System.Drawing.Size(227, 49);
             this.btnEliminar.TabIndex = 14;
             this.btnEliminar.Text = "ELIMINAR";
-            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEliminar.Textcolor = System.Drawing.Color.White;
             this.btnEliminar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
             // Cultivos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnGuardar);
@@ -319,6 +335,7 @@ namespace Vistas
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Cerrar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Cultivos";
             this.Text = "Cultivos";
             ((System.ComponentModel.ISupportInitialize)(this.Cerrar)).EndInit();
